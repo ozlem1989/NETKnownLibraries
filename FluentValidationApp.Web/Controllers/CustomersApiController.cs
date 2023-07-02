1717312696dc5ef825eb9b193f38e56ecd27555a
ozlem.engin <ozlem.engin@turkcell.com.tr>
2023-07-02 10:21:57 +0300

    modalstate auto invalid added and apicontroller added.

---
+++ FluentValidationApp.Web/Controllers/CustomersApiController.cs
@@ -0,0 +1,121 @@
+﻿using FluentValidation;
+using FluentValidationApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationApp.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersApiController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IValidator<Customer> _customerValidator;
+
+        public CustomersApiController(AppDbContext context, IValidator<Customer> customerValidator)
+        {
+            _context = context;
+            _customerValidator = customerValidator;
+        }
+
+        // GET: api/CustomersApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomer()
+        {
+            return await _context.Customer.ToListAsync();
+        }
+
+        // GET: api/CustomersApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            var customer = await _context.Customer.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        // PUT: api/CustomersApi/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CustomersApi
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            var result = _customerValidator.Validate(customer);
+
+            if (!result.IsValid)
+            {
+                return BadRequest(result.Errors.Select(x => new
+                {
+                    property = x.PropertyName,
+                    error = x.ErrorMessage
+                }));
+            }
+
+            _context.Customer.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomer", new { id = customer.Id }, customer);
+        }
+
+        // DELETE: api/CustomersApi/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Customer>> DeleteCustomer(int id)
+        {
+            var customer = await _context.Customer.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customer.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return customer;
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customer.Any(e => e.Id == id);
+        }
+    }
+}
--- FluentValidationApp.Web/Startup.cs
+++ FluentValidationApp.Web/Startup.cs
@@ -1,18 +1,12 @@
-using FluentValidation;
 using FluentValidation.AspNetCore;
-using FluentValidationApp.Web.FluentValidators;
 using FluentValidationApp.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FluentValidationApp.Web
 {
@@ -44,6 +38,11 @@ namespace FluentValidationApp.Web
                 options.RegisterValidatorsFromAssemblyContaining<Startup>();
             });
 
+            // automatically disable Modal state Validation : 
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true; 
+            }); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
